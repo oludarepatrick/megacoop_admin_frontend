609f798b64e27da2661a6b00fafc1799e898c7f2
Ruqayah1204 <mhiedey98@gmail.com>
2025-10-02 21:48:37 +0100

    fix: baseurl

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useAuthStore } from '@/store/authStore';
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000/api',
+    baseURL: import.meta.env.VITE_API_URL,
     headers: {
         'Content-Type': 'application/json',
     }
