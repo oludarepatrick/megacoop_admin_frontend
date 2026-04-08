7bdc6ddde52ddeb3c6bc1a359540242f8dd24efb
Ruqayah1204 <mhiedey98@gmail.com>
2026-04-08 19:53:34 +0100

    dashboard overview adjusted

--- src/types/dashboard.ts
+++ src/types/dashboard.ts
@@ -1,7 +1,7 @@
 export type DashboardStat = {
   total_users: number
   active_loans: number
-  total_savings: string
+  total_savings: number
   total_investments: number
   pending_loans: number
   disbursed_loans: number
